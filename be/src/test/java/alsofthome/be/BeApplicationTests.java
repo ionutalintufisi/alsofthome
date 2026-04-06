ef4eabbc89be6f31cfa840ff4aa3b1658fba4098
ionutalintufisi <ionutalintufisi@gmail.com>
2026-04-06 21:17:42 +0300

    Initial commit

--- be/src/test/java/alsofthome/be/BeApplicationTests.java
+++ be/src/test/java/alsofthome/be/BeApplicationTests.java
@@ -3,6 +3,7 @@ package alsofthome.be;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BeApplicationTests {
